8ee0ec68e5b8bdc6fa1faef7a6c2d95279c6223c
Suphakrit Nakhabat <suphakrit.bas@gmail.com>
2017-07-19 00:51:00 +0700

    add new papple

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var game = new Phaser.Game(w, h, Phaser.CANVAS, 'game');
 var platforms;
 var BASKET = 'basket';
 var APPLE = 'apple';
+var PAPPLE = 'papple';
 var GRASS = 'grass';
 var BOMB = 'bomb';
 var KEEP = 'keep';
@@ -20,6 +21,7 @@ var mainState = {
     preload: function() {
         game.load.image(BASKET, 'assets/basket.png');
         game.load.image(APPLE, 'assets/apple.png');
+        game.load.image(PAPPLE, 'assets/papple.png');
         game.load.image(GRASS, 'assets/grass.png');
         game.load.image(BOMB, 'assets/bomb.png');
         game.load.audio(KEEP, 'assets/keep.wav');
@@ -28,19 +30,22 @@ var mainState = {
 
     create: function() {
         this.score = 0;
-        this.scoreText;
         this.end = false;
+        this.appleSpeed = 200;
+        this.bombSpeed = 300;
 
         this.scoreText = game.add.text(16, 16, 'Score : ' + this.score, {
             fontSize: '20px',
             fill: '#fff'
         });
+
         game.physics.startSystem(Phaser.Physics.ARCADE);
         game.physics.arcade.gravity.y = 100;
 
         platforms = game.add.group();
         platforms.enableBody = true;
         this.apples = game.add.group();
+        this.papples = game.add.group();
         this.bombs = game.add.group();
         this.setupGrass();
 
@@ -50,7 +55,8 @@ var mainState = {
         this.basket.anchor.setTo(0.5);
         this.basket.body.allowGravity = false;
 
-        this.timer = game.time.events.loop(1500, this.addApple, this);
+        game.time.events.loop(1000, this.addApple, this);
+        game.time.events.loop(10000, this.addPapple, this);
         var r = Math.random() * 3000 + 1000;
         game.time.events.loop(r, this.addBomb, this);
 
@@ -63,6 +69,7 @@ var mainState = {
     update: function() {
         if (!this.end) {
             game.physics.arcade.overlap(this.basket, this.apples, this.hitApple, null, this);
+            game.physics.arcade.overlap(this.basket, this.papples, this.hitPapple, null, this);
             game.physics.arcade.overlap(this.basket, this.bombs, this.hitBomb, null, this);
             game.physics.arcade.overlap(this.grass, this.apples, this.gameOver, null, this);
 
@@ -80,19 +87,27 @@ var mainState = {
     },
 
     addApple: function() {
-        var ran = Math.random() * (game.world.width - 40);
+        var ran = Math.random() * (game.world.width - 50);
         var apple = game.add.sprite(ran, -200, APPLE);
         game.physics.enable(apple, Phaser.Physics.ARCADE);
         this.apples.add(apple);
-        apple.body.gravity.y = 200;
+        apple.body.gravity.y = this.appleSpeed++;
+    },
+
+    addPapple: function() {
+        var ran = Math.random() * (game.world.width - 100);
+        var papple = game.add.sprite(ran, -200, PAPPLE);
+        game.physics.enable(papple, Phaser.Physics.ARCADE);
+        this.papples.add(papple);
+        papple.body.gravity.y = this.appleSpeed;
     },
 
     addBomb: function() {
-        var ran = Math.random() * (game.world.width - 40);
+        var ran = Math.random() * (game.world.width - 50);
         var bomb = game.add.sprite(ran, -200, BOMB);
         game.physics.enable(bomb, Phaser.Physics.ARCADE);
         this.bombs.add(bomb);
-        bomb.body.gravity.y = 300;
+        bomb.body.gravity.y = this.bombSpeed++;
     },
 
     hitApple: function(basket, apple) {
@@ -104,6 +119,15 @@ var mainState = {
         }
     },
 
+    hitPapple: function(basket, papple) {
+        if (!this.end) {
+            this.score += 5;
+            this.scoreText.text = 'Score : ' + this.score;
+            papple.destroy();
+            this.keepSound.play();
+        }
+    },
+
     hitBomb: function(basket, bomb) {
         if (!this.end) {
             bomb.destroy();
@@ -111,19 +135,28 @@ var mainState = {
         }
     },
 
-    gameOver: function(grass, apple) {
-        this.end = true;
-        this.expSound.play();
+    updateCount: function() {
+        if (!this.end) {
+            this.time--;
+            this.timeText.text = this.time;
+        }
+    },
 
-        var message = 'Game Over!';
+    gameOver: function(grass, apple) {
+        this.expSound.play();
+        this.end = true;
+        this.textAlert('Game Over!');
+    },
+
+    textAlert: function(message) {
         this.endText = this.add.text(game.world.width / 2, game.world.height / 2 - 20, message, {
             font: '48px serif',
             fill: '#fff'
         });
         this.endText.anchor.setTo(0.5, 0.5);
 
-        message = 'Tab to play';
-        this.playText = this.add.text(game.world.width / 2, game.world.height / 2 + 20, message, {
+        var txtPlay = 'Tab to play';
+        this.playText = this.add.text(game.world.width / 2, game.world.height / 2 + 20, txtPlay, {
             font: '24px serif',
             fill: '#fff'
         });
